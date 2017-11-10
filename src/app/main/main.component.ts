c7af5f1f398a9ad10b8144840a18eee090c9ec06
zacharylangley <zacharylangley@my.unt.edu>
2017-11-10 12:56:58 -0600

    basic css edits

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,7 +10,7 @@ export class MainComponent implements OnInit {
   StartDate = 'February 6th';
   EndDate = 'February 8th';
   RegisterLink = 'https://docs.google.com/forms/d/e/1FAIpQLSfT839wnBDBDl_vPTKDIaAqR73dRQgf-p1Fo5lLemFRj8pD1g/viewform?usp=sf_link';
-  DescriptionText = 'Lorem ipsum dolor sit amet, Vestibulum ut lacus a tellus convallis porta ut eu neque.';
+  DescriptionText = '-----------------    Styling Required    -----------------';
   constructor() { }
 
   ngOnInit() {}
