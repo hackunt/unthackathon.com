e5297b8afa922dc15391cef4e2d27294d8afb90f
zacharylangley <zacharylangley@my.unt.edu>
2017-11-13 10:00:01 -0600

    Logo and Schedule Rough

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -7,6 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
+  SatEvents = [
+    {
+      EventTitle: 'Event Title 1',
+      Location: '555 Filler Street, Denton, Texas',
+      gmapLink: '',
+      Date: 'DayExpanded, MM DD',
+      StartTime: 'hh:mm am/pm',
+      EndTime: 'hh:mm am/pm',
+      Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a orci ac lorem euismod gravida.',
+      URL: 'https://reddit.com/r/unt',
+    },
+    {
+      EventTitle: 'Event Title 2',
+      Location: '555 Filler Street, Denton, Texas',
+      gmapLink: '',
+      Date: 'DayExpanded, MM DD',
+      StartTime: 'hh:mm am/pm',
+      EndTime: 'hh:mm am/pm',
+      Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a orci ac lorem euismod gravida.',
+      URL: 'https://reddit.com/r/unt',
+    },
+    {
+      EventTitle: 'Event Title 3',
+      Location: '555 Filler Street, Denton, Texas',
+      gmapLink: '',
+      Date: 'DayExpanded, MM DD',
+      StartTime: 'hh:mm am/pm',
+      EndTime: 'hh:mm am/pm',
+      Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a orci ac lorem euismod gravida.',
+      URL: 'https://reddit.com/r/unt',
+    },
+    {
+      EventTitle: 'Event Title 4',
+      Location: '555 Filler Street, Denton, Texas',
+      gmapLink: '',
+      Date: 'DayExpanded, MM DD',
+      StartTime: 'hh:mm am/pm',
+      EndTime: 'hh:mm am/pm',
+      Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a orci ac lorem euismod gravida.',
+      URL: 'https://reddit.com/r/unt',
+    },
+    {
+      EventTitle: 'Event Title 5',
+      Location: '555 Filler Street, Denton, Texas',
+      gmapLink: '',
+      Date: 'DayExpanded, MM DD',
+      StartTime: 'hh:mm am/pm',
+      EndTime: 'hh:mm am/pm',
+      Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a orci ac lorem euismod gravida.',
+      URL: 'https://reddit.com/r/unt',
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
