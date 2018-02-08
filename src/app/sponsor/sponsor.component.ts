63ab8249cab887b1f25b04c7d9392acadd3e27e0
mcjcloud <brayden14cloud@gmail.com>
2018-02-08 00:04:36 -0600

    Added typeform and removed links to unfinished parts of the site.

--- src/app/sponsor/sponsor.component.ts
+++ src/app/sponsor/sponsor.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SponsorComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+  }
 
   ngOnInit() {
+    
   }
 
 }
